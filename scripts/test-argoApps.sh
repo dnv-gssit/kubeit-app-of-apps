376de167b2fb5f10ffbba4d842d9a66bb975ad1e
mikolajmdnv <Mikolaj.Miotk@dnv.com>
2022-08-17 12:25:38 +0200

    subchart version

---
+++ scripts/test-argoApps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+RUN_PATH=$PWD
+SCRIPT_PATH=${SCRIPT_PATH:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}
+
+RENDERED_CHART_PATH=/tmp/rendered_chart
+
+echo "===]> Info: RUN_PATH=$RUN_PATH"
+echo "===]> Info: SCRIPT_PATH=$SCRIPT_PATH"
+
+rm -rf $RENDERED_CHART_PATH
+
+rm -rf charts Chart.lock
+
+helm template --dependency-update --render-subchart-notes .  \
+  -f $SCRIPT_PATH/test-argoApps.yaml \
+  --output-dir $RENDERED_CHART_PATH --debug
+
+yamllint -c $SCRIPT_PATH/../.yamllint -s $RENDERED_CHART_PATH
